e4518e989d4dc6b48f6a824c24dfc485b137eb56
devSirNaelC7 <sirnaelc7@gmail.com>
2023-04-09 20:55:12 -0300

    feat: html page for data visualization

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ createServer(async (request, response) => {
         }))
         .pipeTo(new WritableStream({
             async write(chunk) {
-                await setTimeout(1000)
+                await setTimeout(200)
                 items++
                 response.write(chunk)
             },
